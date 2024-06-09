36e2eebdcd84361adf57a6988d791192eb20abfa
Erdembileg Ariunbold <97537483+Zwique@users.noreply.github.com>
2024-06-09 14:56:46 +0800

    Update Z_Cipher.py

--- Z_Cipher.py
+++ Z_Cipher.py
@@ -1,6 +1,12 @@
-import pyfiglet
+import os
 import random
-import termcolor
+
+try:
+    import termcolor
+    import pyfiglet
+except:
+    os.system("pip3 install termcolor")
+    os.system("pip3 install pyfiglet")
 
 BANNER = pyfiglet.figlet_format("Z_Cipher",font="slant")
 COLOR = termcolor.colored(BANNER, "blue")
